184816703143568ae9c187e8b68b9f7acb8d8adf
acolyte0 <61568999+acolyte0@users.noreply.github.com>
2020-03-19 01:09:05 +0900

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,95 @@
+const ppl = [
+    {
+        name:'Aaron',
+        gender: 'male',
+        salary: 10,
+        friends: [
+            {
+                name:'Mike',
+                gender: 'male',
+                salary: 10,
+                friends: [
+                    {
+                        name:'Betty',
+                        gender: 'female',
+                        salary: 100,
+                        friends: []
+                    }
+                ]
+            },
+            {
+                name:'Sharon',
+                gender: 'female',
+                salary: 1000,
+                friends: []
+            },
+            {
+                name:'Gary',
+                gender: 'male',
+                salary: 100,
+                friends: []
+            }
+
+        ]
+    },
+    {
+        name:'Charles',
+        gender: 'male',
+        salary: 1000,
+        friends: [
+            {
+                name:'Zoe',
+                gender: 'male',
+                salary: 1,
+                friends: []
+            },
+            {
+                name:'Donna',
+                gender: 'female',
+                salary: 1,
+                friends: []
+            }
+
+        ]
+    },
+    {
+        name:'Sally',
+        gender: 'female',
+        salary: 10000,
+        friends: []
+    }
+];
+
+function first(people, total) {
+    if(!Array.isArray(people)) {
+        for(p in people) {
+            if(p == 'name') {
+                console.log("not array " + people[p] + " \n");
+            }
+            if(p == 'salary') {
+                total += people[p];
+                console.log("line71-" + total);
+            }
+
+            if(p == 'friends' && typeof people[p][0] != 'undefined') {
+                for(v1 of people[p]){
+                    total = first(v1, total);
+                    console.log("line77-" + total);
+
+                }
+            }
+
+        }
+    } else {
+        for(v1 of people){
+            console.log("array " + v1 + " \n");
+            total = first(v1, total);
+            console.log("line87-" + total);
+
+        }
+    }
+
+    return total;
+}
+
+console.log(first(ppl, 0));
